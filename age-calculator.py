936ce9335afb7487bc59ef7fd6e293fab74e3387
Ngwu <obiora.ngwu@ubagroup.com>
2018-03-23 16:26:55 +0100

    my age

--- age-calculator.py
+++ age-calculator.py
@@ -1,3 +1,4 @@
 print("I will be 100 years old in the year 2080")
 name = input("what is your name: ")
-print(name + " will be 89 in the year 2080")
+age = str(input("what is your age: "))
+print(name + " will be" + age + "years old in 2080")
